6510cc557fa289aab1a13878dd93f91ecb69b073
Max Geißler <max.geissler@stud.th-deg.de>
2023-08-09 13:48:33 +0200

    Upload New File

---
+++ misc/extract_MatLabScores2csv.py
@@ -0,0 +1,43 @@
+import os
+import scipy.io
+import csv
+
+
+def save_sorttable_to_csv(mat_file_path, output_directory):
+    mat = scipy.io.loadmat(mat_file_path)
+
+    if 'sortTable' not in mat:
+        print(f"Error: '{mat_file_path}' does not contain 'sortTable' key. Skipping...")
+        return
+
+    sort_table = mat['sortTable'][0]
+
+    base_name = os.path.splitext(os.path.basename(mat_file_path))[0]
+    csv_file_path = os.path.join(output_directory, base_name + '.csv')
+
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        # writer.writerow(["Image Name", "Vote", "Duration", "Timestamp"])
+        for i in range(len(sort_table)):
+            image_name = mat['imgList'][0][i]
+            vote = mat['voteList'][0][i]
+            duration = mat['drtList'][0][i]
+            timestamp = mat['voteTime'][0][i]
+
+            writer.writerow([image_name[0], vote, duration[0], timestamp[0]])
+
+        print(f"CSV file saved at: {csv_file_path}")
+
+
+def process_directory(directory, output_directory):
+    for file_name in os.listdir(directory):
+        if file_name.endswith('.mat'):
+            mat_file_path = os.path.join(directory, file_name)
+            save_sorttable_to_csv(mat_file_path, output_directory)
+
+
+# Provide the directory path containing the .mat files
+directory = '/home/maxgan/WORKSPACE/UNI/BA/TIQ/Assets/DatasetSubjective/scores/Persons/allMatlab'
+output_directory = '/home/maxgan/WORKSPACE/UNI/BA/TIQ/Assets/DatasetSubjective/scores/Persons/allMatlab'
+
+process_directory(directory, output_directory)
