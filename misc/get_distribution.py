fec97d6cbf1733b46738a9f480cfb26397252759
Max Geißler <max.geissler@stud.th-deg.de>
2023-10-09 16:36:13 +0200

    refactored

--- misc/get_distribution.py
+++ misc/get_distribution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-# Read the CSV file
-csv_path = "./assets/Test/AccTestCsv/Obs0AccTest.csv"
-data = pd.read_csv(csv_path, header=0)
+# Directory containing the CSV files
+csv_directory = "assets/Test/AccTestCsv"
 
-# Map rating values to their corresponding labels
+# Get a list of all CSV files in the directory
+csv_files = [file for file in os.listdir(csv_directory) if file.endswith(".csv")]
+
+# Define rating labels
 rating_labels = {
     1: "Bad",
     2: "Insufficient",
@@ -14,34 +17,44 @@ rating_labels = {
     5: "Excellent"
 }
 
-data["Rating_Label"] = data["Vote"].map(rating_labels)
-print(data)
-# Group data by Rating_Label and count occurrences
-class_counts = data["Rating_Label"].value_counts()
+# Function to process a single CSV file
+def process_csv(csv_file):
+    # Read the CSV file
+    data = pd.read_csv(os.path.join(csv_directory, csv_file), header=0)
+    
+    # Map rating values to their corresponding labels
+    data["Rating_Label"] = data["Vote"].map(rating_labels)
 
-# Reindex the class_counts Series to match the desired order
-class_counts = class_counts.reindex(rating_labels.values())
-# print(class_counts)
+    # Group data by Rating_Label and count occurrences
+    class_counts = data["Rating_Label"].value_counts()
 
-# Calculate total number of images
-total_images = class_counts.sum()
+    # Reindex the class_counts Series to match the desired order
+    class_counts = class_counts.reindex(rating_labels.values())
 
-# Create a bar chart
-plt.figure(figsize=(10, 6))
-class_counts.plot(kind="bar", color='skyblue')
-plt.title("Image Rating Distribution ObsX (obs1)")
-plt.xlabel("Rating")
-plt.ylabel("Number of Images")
-plt.xticks(rotation=45)
-plt.tight_layout()
+    # Calculate total number of images
+    total_images = class_counts.sum()
 
-# Save the bar chart as an image
-output_image_path = "./assets/Test/AccTestCsv/rating_distribution_ObsX_obs0.png"
-plt.savefig(output_image_path)
+    # Create a bar chart
+    plt.figure(figsize=(10, 6))
+    class_counts.plot(kind="bar", color='skyblue')
+    plt.title(f"Image Rating Distribution for {csv_file.split('.')[0]}")
+    plt.xlabel("Rating")
+    plt.ylabel("Number of Images")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
 
-# Display the table
-print("Rating Distribution Table:")
-print(class_counts)
+    # Save the bar chart as an image
+    output_image_path = os.path.join(csv_directory, f"rating_distribution_{csv_file.replace('.csv', '.png')}")
+    plt.savefig(output_image_path)
 
-# Close the plot
-plt.close()
+    # Display the table
+    print(f"Rating Distribution Table for {csv_file.split('.')[0]}")
+    print(class_counts)
+
+    # Close the plot
+    plt.close()
+
+# Process each CSV file in the directory
+for csv_file in csv_files:
+    process_csv(csv_file)
+
