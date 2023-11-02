4d7d25515f95dd52e77b564dac7f60edf66f4e6b
Max Geißler <max.geissler@stud.th-deg.de>
2023-11-02 10:38:25 +0100

    documentation

--- utils/imageset_handler.py
+++ utils/imageset_handler.py
@@ -1,21 +1,37 @@
 import os
 import random
-import re
 import pandas as pd
 from torch.utils.data import Dataset
 from PIL import Image
 
 class ImageQualityDataset(Dataset):
     def __init__(self, csv_file: str, image_dir: str, transform=None):
+        """
+        Custom dataset class for image quality prediction.
+
+        This class reads a CSV file with image names and votes (quality levels), and loads images
+        from the specified directory. It shuffles and selects images to balance the dataset.
+
+        Parameters:
+        csv_file (str): Path to the CSV file containing image information.
+        image_dir (str): Directory path containing image files.
+        transform (callable, optional): A function/transform to apply to the loaded image.
+
+        Output:
+        - An instance of the ImageQualityDataset class.
+
+        Note:
+        - The CSV file should have a 'Image Name' or 'Vote' column.
+        """
         self.csv_file = csv_file
         self.transform = transform
 
         df = pd.read_csv(csv_file)
 
-        # Check if the CSV file has a header row
-        if "Image Name" in df.columns and "Quality Level" in df.columns:
+        # Determine column names based on the header presence
+        if "Image Name" in df.columns and "Vote" in df.columns:
             image_column = "Image Name"
-            quality_column = "Quality Level"
+            quality_column = "Vote"
         else:
             image_column = df.columns[0]
             quality_column = df.columns[1]
@@ -24,7 +40,7 @@ class ImageQualityDataset(Dataset):
 
         self.image_files = []
         for _, row in df.iterrows():
-            image_path = os.path.join(image_dir,row[image_column])
+            image_path = os.path.join(image_dir, row[image_column])
             quality_level = int(row[quality_column]) - 1
             self.image_files.append((image_path, quality_level))
             class_counts[quality_level] += 1
@@ -40,25 +56,12 @@ class ImageQualityDataset(Dataset):
 
         print(f"Number of images in the dataset: {len(selected_files)}")
 
-    
-    def get_true_quality_level(self, image_name):
-        # Use regular expression to extract the quality level from the image name
-        # For example, from "12ILSVRC2013_train_00000419.JPEG_I1_Q6.jpeg",
-        # we want to extract the "1" after the second 'I', which indicates the quality level.
-        match = re.search(r'_I(\d+)_', image_name)
-        if match:
-            quality_level = match.group(1)
-            return int(quality_level) - 1
-        else:
-            print(f"No Quality-Level found for {image_name}")
-
     def __len__(self):
         return len(self.image_files)
 
     def __getitem__(self, idx):
         image_path, label = self.image_files[idx]
         # Load image and apply transformations
-        # print(image_path)
         image = Image.open(image_path).convert('RGB')
         if self.transform:
             image = self.transform(image)
